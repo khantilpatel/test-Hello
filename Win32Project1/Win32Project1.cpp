846f5c8c7b1f1408a7cc090589522a0f21a86be7
khantilpatel <khantilpatel@gmail.com>
2014-07-02 20:29:43 -0600

    test change

--- Win32Project1/Win32Project1.cpp
+++ Win32Project1/Win32Project1.cpp
@@ -18,6 +18,7 @@ LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPTSTR    lpCmdLine,
                      _In_ int       nCmdShow)
